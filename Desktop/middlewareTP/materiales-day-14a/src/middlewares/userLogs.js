3b8cbb5995b1ada6031b20f0918a0c66db742d1c
PaulaEscobar <aanitaescobar@gmail.com>
2024-01-08 22:14:44 -0300

    se integro el paso2

--- Desktop/middlewareTP/materiales-day-14a/src/middlewares/userLogs.js
+++ Desktop/middlewareTP/materiales-day-14a/src/middlewares/userLogs.js
@@ -4,14 +4,13 @@ const path = require('path');
 const logs = path.join(__dirname, '../logs/userLogs.txt'); 
 
 function userLogs(req, res, next) {
-    
+
 const ingresoUrl= `el ususario ingreso a la ruta: ${req.url} \n`
 fs.appendFileSync(logs, ingresoUrl);
     
     next();
     
 }
-
 
 
 module.exports = userLogs 
