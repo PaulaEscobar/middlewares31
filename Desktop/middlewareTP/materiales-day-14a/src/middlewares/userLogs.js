aa1fb66863b5f7e32b737d64e6b8fc523929ece4
PaulaEscobar <aanitaescobar@gmail.com>
2024-01-08 21:26:04 -0300

    modifique

--- Desktop/middlewareTP/materiales-day-14a/src/middlewares/userLogs.js
+++ Desktop/middlewareTP/materiales-day-14a/src/middlewares/userLogs.js
@@ -4,8 +4,9 @@ const path = require('path');
 const logs = path.join(__dirname, '../logs/userLogs.txt'); 
 
 function userLogs(req, res, next) {
-   const ab = `el ususario ingreso a la ruta: ${req.url} \n`
-   fs.appendFileSync(logs, ab);
+    
+const ingresoUrl= `el ususario ingreso a la ruta: ${req.url} \n`
+fs.appendFileSync(logs, ingresoUrl);
     
     next();
     
--- Desktop/middlewareTP/materiales-day-14a/src/routes/main.js
+++ Desktop/middlewareTP/materiales-day-14a/src/routes/main.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 // const userLogs  = require('../middlewares/userLogs');
+
 const mainConrtroller = require('../controllers/mainController')
 
 router.get('/', mainConrtroller.index)
+
 
 
 
